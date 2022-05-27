af3c3f23bd6cc55c467e26e8591d18677ecb6d77
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-05-27 15:20:50 +0900

    Update Prog72Test.java

--- src/test/java/Prog72Test.java
+++ src/test/java/Prog72Test.java
@@ -1,7 +1,12 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import java.io.*;
-
+/**
+ * @version (20220526)
+ * 
+ * (注意) Prog72クラス内に main() が
+ * 　　　　宣言されるまで、このテストクラスはエラーが表示される
+ **/
 public class Prog72Test {
 
     @Test
@@ -18,10 +23,14 @@ public class Prog72Test {
         Prog72.main(null);
 
         // assertion
-        String[] prints = bos.toString().split("\n");
-        assertEquals("お化けキノコAは逃げ出した!", prints[0]);
-        assertEquals("お化けキノコBは自爆した!", prints[1]);
-
+        String[] prints = bos.toString().split("\r\n|\n");
+        try {
+            assertEquals("お化けキノコAは逃げ出した！", prints[0],"実行結果の１行目が実行例と異なります!");
+            assertEquals("お化けキノコBは自爆した！", prints[1],"実行結果の２行目が実行例と異なります!");
+        } catch (AssertionError err) {
+            System.setOut(originalOut);
+            throw err;
+        } 
         // undo the binding in System
         System.setOut(originalOut);
     }
