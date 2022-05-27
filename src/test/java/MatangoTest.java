748e6fe12ff4d1ae5f8b7d4aa188530424070dcb
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-05-27 15:20:11 +0900

    Update MatangoTest.java

--- src/test/java/MatangoTest.java
+++ src/test/java/MatangoTest.java
@@ -1,7 +1,11 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 import java.io.*;
-
+/**
+ * @version (20220526)
+ *  注）感嘆符は全角文字とする
+ **/
 public class MatangoTest {
     @Test
     public void testSelfDestruct()
@@ -17,12 +21,20 @@ public class MatangoTest {
         m.selfDestruct();
 
         // assertion
-        assertEquals(0, m.hp);
-        assertEquals("お化けキノコGは自爆した!\n", bos.toString());
+        try {
+            assertEquals(0, m.hp,"Matango.selfDestruct()のhp設定が不正です!");
+            String[] prints = bos.toString().split("\r\n|\n");
+            assertEquals("お化けキノコGは自爆した！", prints[0], "Matango.selfDestruct()のprint出力が不正です!");
+        } catch (AssertionError err) {
+            System.setOut(originalOut);
+            throw err;
+        } catch (ArrayIndexOutOfBoundsException excpt) {
+            System.setOut(originalOut);
+            fail("コード中に適切なprint命令がありません!");
+        }
 
         // undo the binding in System
         System.setOut(originalOut);
-
     }
 
     @Test
@@ -39,11 +51,18 @@ public class MatangoTest {
         m.run();
 
         // assertion
-        assertEquals("お化けキノコGは逃げ出した!\n", bos.toString());
+        try {
+            String[] prints = bos.toString().split("\r\n|\n");
+            assertEquals("お化けキノコGは逃げ出した！", prints[0], "Matango.run()のprint出力が不正です!");
+        } catch (AssertionError err) {
+            System.setOut(originalOut);
+            throw err;
+        } catch (ArrayIndexOutOfBoundsException excpt) {
+            System.setOut(originalOut);
+            fail("コード中に適切なprint命令がありません!");
+        }
 
         // undo the binding in System
         System.setOut(originalOut);
     }
-
-
 }
